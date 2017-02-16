577a73203bd7ef140c2123cb1357f2d68bdf7318
lammn <mnlam@lockon-vn.com>
2017-02-16 17:37:26 +0700

    add event file

---
+++ Event.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * This file is part of the Coupon plugin
+ *
+ * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\CointCheck;
+
+use Eccube\Application;
+use Eccube\Event\TemplateEvent;
+use Plugin\CointCheck\Util\Version;
+
+/**
+ * Class Event.
+ */
+class Event
+{
+    /** @var \Eccube\Application */
+    private $app;
+
+    /**
+     * Event constructor.
+     *
+     * @param Application $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     *
+     * @param TemplateEvent $event
+     */
+    public function onRenderShoppingIndex(TemplateEvent $event)
+    {
+        $app = $this->app;
+        $parameters = $event->getParameters();
+        if (is_null($parameters['Order'])) {
+            return;
+        }
+
+        // 登録がない、レンダリングをしない
+        /* @var $Order \Eccube\Entity\Order */
+        $Order = $parameters['Order'];
+        $payment = $Order->getPaymentMethod();
+        $source = null;
+        if ($payment == 'ビットコイン決済') {
+            // このタグを前後に分割し、間に項目を入れ込む
+            // 元の合計金額は書き込み済みのため再度書き込みを行う
+            $snipet = $app['twig']->getLoader()->getSource('Coupon/Resource/template/default/discount_shopping_item.twig');
+            $search = '<div id="summary_box__result" class="total_amount">';
+            $replace = $search.$snipet;
+            $source = str_replace($search, $replace, $source);
+            $parameters['Order'] = $Order;
+        }
+        //$event->setSource($source);
+    }
+}
--- Resource/doctrine/migration/Version20171502000000.php
+++ Resource/doctrine/migration/Version20171502000000.php
@@ -13,6 +13,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 use Eccube\Application;
+use Eccube\Common\Constant;
 
 /**
  * Version20171502000000.
@@ -34,6 +35,7 @@ class Version20171502000000 extends AbstractMigration
             ->select('max(p.id)')
             ->getQuery()
             ->getSingleResult();
+        //get max id of payment table and plus 1 for new id of bitcoint.
         $max = $entities[1] + 1;
         $this->addSql("INSERT INTO dtb_payment (payment_id, payment_method, charge, rule_max, rank, fix_flg, del_flg, creator_id, create_date, update_date, payment_image, charge_flg, rule_min) VALUES ($max, 'ビットコイン決済', 0, NULL, 1, 1, 0, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 1, 0);");
         $this->createTable($schema);
@@ -55,7 +57,16 @@ class Version20171502000000 extends AbstractMigration
             }
             $app = Application::getInstance();
             $repository = $app['orm.em']->getRepository('Eccube\Entity\Payment');
+            /* @var $Payment \Eccube\Entity\Payment */
             $Payment = $repository->findOneBy(array('method' => 'ビットコイン決済'));
+            $repository = $app['orm.em']->getRepository('Eccube\Entity\PaymentOption');
+            //remove payment option
+            $PaymentOptions = $repository->findBy(array('Payment' => $Payment));
+            foreach ($PaymentOptions as $option) {
+                $app['orm.em']->remove($option);
+                $app['orm.em']->flush($option);
+            }
+            //remove payment
             $app['orm.em']->remove($Payment);
             $app['orm.em']->flush($Payment);
         }
